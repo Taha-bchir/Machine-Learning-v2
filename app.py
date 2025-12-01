3acec11a8ba7e885ba330c35e071094de25bb115
Taha Bchir <147826212+Taha-bchir@users.noreply.github.com>
2025-12-02 00:31:58 +0100

    Remove unused joblib import from app.py
    Removed unused import of joblib.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
