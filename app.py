65f3700740a3174cb513523fd002351ced3aa31b
Taha Bchir <147826212+Taha-bchir@users.noreply.github.com>
2025-12-02 00:33:53 +0100

    Remove unused sklearn imports
    Removed unused imports for cleaner code.

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
