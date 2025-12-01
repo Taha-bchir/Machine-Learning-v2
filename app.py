1c0982439e9d1326dd006b79b02f2ebcce8f8926
Taha Bchir <147826212+Taha-bchir@users.noreply.github.com>
2025-12-02 00:34:20 +0100

    Remove unused imports from app.py
    Removed unused imports for matplotlib, seaborn, and os.

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
 
 st.set_page_config(
     page_title="Prediction Performance Candidats RH",
